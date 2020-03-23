c226c1d90793f53b092815d2f8bb76cdeedeaf3c
LTDestiny <395936523@qq.com>
2020-03-23 18:13:17 +0800

    八皇后问题

--- src/com/scarlett/op/Recursion.java
+++ src/com/scarlett/op/Recursion.java
@@ -9,6 +9,15 @@ package com.scarlett.op;
 public class Recursion {
 
     /**
+     * 定义一个max表示共有多少个皇后
+     */
+    int max = 8;
+    /**
+     * 定义一个数组arr，保存皇后存放位置的结果，比如arr[8] = {0, 4, 7, 5, 2, 6, 1, 3}
+     */
+    int[] array = new int[max];
+
+    /**
      * 阶乘
      *
      * @param n 求n的阶乘
@@ -35,19 +44,26 @@ public class Recursion {
      * @return 如果找到通路，返回true，否则返回false
      */
     private static boolean setWay(int[][] map, int i, int j) {
-        if (map[6][5] == 2) {   // 出口已找到
+        // 出口已找到
+        if (map[6][5] == 2) {
             return true;
         } else {
-            if (map[i][j] == 0) {   // 如果当前这个点没有走过
+            // 如果当前这个点没有走过
+            if (map[i][j] == 0) {
                 // 按照策略 下->右->上->左
-                map[i][j] = 2;  // 假定该店是可以走通
-                if (setWay(map, i + 1, j)) {    // 向下走
+                // 假定该点是可以走通
+                map[i][j] = 2;
+                // 向下走
+                if (setWay(map, i + 1, j)) {
                     return true;
-                } else if (setWay(map, i, j + 1)) {    // 向右走
+                    // 向右走
+                } else if (setWay(map, i, j + 1)) {
                     return true;
-                } else if (setWay(map, i - 1, j)){  // 向上
+                    // 向上
+                } else if (setWay(map, i - 1, j)){
                     return true;
-                } else if (setWay(map, i, j - 1)) { // 向走
+                    // 向左走
+                } else if (setWay(map, i, j - 1)) {
                     return true;
                 } else {
                     // 说明该点走不通，是死路
@@ -56,6 +72,57 @@ public class Recursion {
                 }
             } else {
                 return false;
+            }
+        }
+    }
+
+    /**
+     * 将皇后摆放的位置输出
+     */
+    private void print() {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * 查看当我们防止第n个皇后，就去检测该皇后是否和前面已经摆放的皇后冲突
+     * @param n 第个皇后
+     * @return false 冲突
+     */
+    private boolean judge(int n) {
+        for (int i = 0; i < n; i++) {
+            // array[i] == array[n] 表示判断第n个皇后是否和前面的n-1个皇后在同一列
+            // Math.abs(n - i) == Math.abs(array[n] - array[i]) 表示判断第n个皇后是否和第i个皇后是否在同一斜线
+            if (array[i] == array[n] || Math.abs(n - i) == Math.abs(array[n] - array[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 八皇后
+     * 在8×8格的国际象棋上摆放八个皇后，使其不能互相攻击，
+     * 即任意两个皇后都不能处于同一行、同一列或同一斜线上，问有多少种摆法
+     * 一共有92种解法
+     * @param n 第n个皇后
+     */
+    private void queen8(int n) {
+        // n = 8, 8个皇后已经放好
+        if (n == max) {
+            print();
+            return;
+        }
+        // 依次放入皇后，并判断是否冲突
+        for (int i = 0; i < max; i++) {
+            // 先把当前这个皇后n，放到该行的第1列
+            array[n] = i;
+            // 判断当放置第n个皇后到第i列时，是否冲突
+            if (judge(n)) {
+                // 接着放n+1个皇后，开始递归
+                queen8(n + 1);
             }
         }
     }
@@ -97,5 +164,10 @@ public class Recursion {
             }
             System.out.println();
         }
+
+        // 测试八皇后
+        System.out.println("测试八皇后");
+        Recursion recursion = new Recursion();
+        recursion.queen8(0);
     }
 }
