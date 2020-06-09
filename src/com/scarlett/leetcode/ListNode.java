490a5d31b76aa9d92247975d398c1a01bf469e0a
LTDestiny <395936523@qq.com>
2020-06-09 12:37:45 +0800

    leetcode第二题

---
+++ src/com/scarlett/leetcode/ListNode.java
@@ -0,0 +1,17 @@
+package com.scarlett.leetcode;
+
+/**
+ * @ClassName : LinkNode
+ * Description :
+ * @Author : scarlett
+ * @Date: 2020-06-09 10:51
+ */
+public class LinkNode {
+
+    int val;
+    LinkNode next;
+
+    LinkNode(int x) {
+        val = x;
+    }
+}
---
+++ src/com/scarlett/leetcode/Solution2.java
@@ -0,0 +1,10 @@
+package com.scarlett.leetcode;
+
+/**
+ * @ClassName : Solution2
+ * Description :
+ * @Author : scarlett
+ * @Date: 2020-06-09 10:48
+ */
+public class Solution2 {
+}
